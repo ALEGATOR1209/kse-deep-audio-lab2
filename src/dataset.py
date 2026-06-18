605a85b586ab6beafeeca9c67e473649054ce03a
Oleh Kovalyshyn <oleh.kovalyshyn.25@kse.org.ua>
2026-06-18 22:35:01 +0300

    collate function

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 from torch.nn.utils.rnn import pad_sequence
 from pathlib import Path
+from math import floor, ceil
 import pandas as pd
 import librosa
 import torch
@@ -78,3 +79,42 @@ class VoxConverseDataset(Dataset):
       ann: audio_by_stem[ann.stem]
       for ann in self.annotation_files
     }
+
+def make_collate_fn(hop: int, sample_rate: int):
+  hop_seconds = hop / sample_rate
+
+  def collate_fn(batch):
+    waveforms = [torch.as_tensor(w, dtype=torch.float32) for w, _ in batch]
+    wav_lengths = [len(w) for w in waveforms]
+    waveforms = pad_sequence(waveforms, batch_first=True)
+
+    max_size = waveforms.shape[-1]
+    n_frames = ceil(max_size / hop)
+
+    labels = torch.zeros(len(batch), n_frames)
+    mask = torch.zeros(len(batch), n_frames, dtype=torch.bool)
+
+    for i, ((turns, _), wl) in enumerate(zip(batch, wav_lengths)):
+      # calculate number of unpadded samples
+      n_valid = ceil(wl / hop)
+
+      # set unpadded samples as true, paddings are false
+      mask[i, :n_valid] = True
+
+      for t in turns:
+        # calculate start sample of the speech turn
+        start = floor(t.start / hop_seconds)
+        start = max(0, start)
+
+        # calculate end sample of the speech turn
+        end = t.start + t.duration
+        end = floor(end / hop_seconds)
+        end = min(n_valid, end)
+
+        # label samples between start and end as speech.
+        # non-speech samples would remain 0
+        labels[i, start:end] = 1.0
+
+    return waveforms, labels, mask
+
+  return collate_fn
